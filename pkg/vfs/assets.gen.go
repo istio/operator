fd22ace0f2a4c1055965a387d7ff29a3eeb032e3
Xinnan Wen <iamwen@google.com>
2019-11-18 14:38:45 -0800

    update minimal profile (#620) (#621)
    * update minimal profile
    
    * update gen
    
    * update goldens

--- pkg/vfs/assets.gen.go
+++ pkg/vfs/assets.gen.go
@@ -37748,14 +37748,18 @@ spec:
   values:
     global:
       useMCP: false
+      controlPlaneSecurityEnabled: false
       proxy:
         envoyStatsd:
           enabled: false
           host:
           port:
+      mtls:
+        auto: false
 
     pilot:
       sidecar: false
+      useMCP: false
 
     prometheus:
       enabled: false
